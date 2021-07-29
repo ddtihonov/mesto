c8cbdde84d4138c87bd72372a2dc9c2dc4069fc7
Dmitry Tikhonov <ddtihonov@yandex.ru>
2021-07-29 23:31:36 +0300

    2 попапа

--- scripts/index.js
+++ scripts/index.js
@@ -26,6 +26,8 @@ const initialCards = [
 ]; 
 
 const popup = document.querySelector('.popup');
+const popupСhangeCell = document.querySelector('.popup-cells');
+let formElement = document.querySelector('.popup__container');
 let nameInput = document.querySelector('.popup__input_user_name');
 let jobInput = document.querySelector('.popup__input_user_job');
 let profileName = document.querySelector('.profile__name');
@@ -49,17 +51,29 @@ document.querySelectorAll('.cell__basket').forEach (button => {
         evt.target.parentElement.remove();
     })});    
 
+// открытие popup данные пользователя
 function togglePopup() {
     popup.classList.toggle('popup_opened');
     if(popup.classList.contains('popup_opened')){
         nameInput.value = profileName.textContent
         jobInput.value = profileProfession.textContent
     }
-    
+}
+
+// отурытие popup новая карточка
+function togglePopupCell () {
+    popupСhangeCell.classList.toggle('popup_opened');
 }
 
 document.querySelector('.profile__changes-button').addEventListener('click', togglePopup);
-document.querySelector('.popup__close-icon').addEventListener('click', togglePopup);
+document.querySelector('.profile__add-button').addEventListener('click', togglePopupCell);
+
+//закрытие всех popup
+document.querySelectorAll('.popup__close-icon').forEach (button => {
+    button.addEventListener('click', (evt) => {
+        evt.target.closest('.popup').toggle('popup_opened');
+    })});  
+
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
