ac5953840dd2e524eef31c3e98b72c7fab4cb5c7
Dmitry Tikhonov <ddtihonov@yandex.ru>
2021-09-21 22:29:45 +0300

    вебпак

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import './index.css';
+
 import {initialCards} from '../utils/initial-cards.js';
 import Card from '../components/Card.js';
 import FormValidator from '../components/FormValidator.js';
